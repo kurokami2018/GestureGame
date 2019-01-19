32fc47d03c9f634efa7ec0c5d730eda138f3c7f2
konnoyuma <g17930ky@gm.tsuda.ac.jp>
2019-01-19 18:27:37 +0900

    Main.javaを書き換えてfxmlが表示されるようになりました。fxmlの編集がまだよくわかんないです。

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,34 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("BattleMode.fxml"));
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
+	}
+	public static void EyeDetection() {
+
+	}
+	public static void MouseDetection() {
+
 	}
 }
